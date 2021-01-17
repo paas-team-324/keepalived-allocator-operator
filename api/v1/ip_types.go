4855796e4a0bd2435534554f0a275a49789dcf3a
Vlad <vlad.pbr@gmail.com>
2021-01-17 13:14:29 +0200

    feat(cleaned IP resource status)

--- api/v1/ip_types.go
+++ api/v1/ip_types.go
@@ -29,8 +29,6 @@ type IPSpec struct {
 
 // IPStatus defines the observed state of IP
 type IPStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
 // +kubebuilder:object:root=true
